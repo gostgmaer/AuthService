39b891cdbe4bd63025888e020bef1423d3ce9859
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-03-26 15:03:59 +0530

    update backednd

---
+++ loginbackend/config.js
@@ -0,0 +1,5 @@
+export default  {
+  ATLAS_URI: "mongodb+srv://kishor81160:iWbZUBngkch8Vxfx@cluster0.wctmvxp.mongodb.net/?retryWrites=true&w=majority",
+  EMAIL:'ole.bogisich@ethereal.email',
+  PASSWORD:'j2usVU9hwBFJrtPrWf'
+};
